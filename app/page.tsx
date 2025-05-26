43e2d1915bcc2697770fddb186a22dbc3383389b
rsosisnsusjs <rutchaputch47@gmail.com>
2025-05-26 21:31:45 +0700

    Ver0.1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function HomePage() {
   return (
     <div className="text-center p-20">
-      <h1 className="text-4xl font-bold">Welcome to VALODIWA</h1>
+      <h1 className="text-4xl font-bold">Welcome to VALODIWA Ver0.1</h1>
     </div>
   )
 }
